78aacbe0dba85a8db9837c51dfc6f52dfb9ada9e
SagarSharma2999 <64771354+SagarSharma2999@users.noreply.github.com>
2023-04-24 17:06:55 +0530

    Add files via upload

---
+++ Star.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestProj
+{
+    public class Star
+    {
+        private double _shineFactor;
+        private bool IsFaded = false;
+        public string Name { get; set; } = string.Empty;
+        public Star(double ShineFactor) => _shineFactor = ShineFactor;
+        public void FadeOut() => IsFaded = true;
+        public double Shine()
+        {
+            if (IsFaded)
+            {
+                throw new InvalidOperationException("Star cannot shine when faded");
+            }
+            return _shineFactor;
+        }
+    }
+}
